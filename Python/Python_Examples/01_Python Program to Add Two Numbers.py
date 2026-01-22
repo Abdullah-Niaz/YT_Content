ec31dac5ee5751d53edbd47717f46fa156136179
Abdullah Niaz <abdullahniaz423@gmail.com>
2026-01-22 11:49:35 +0500

    add two numbers

---
+++ Python/Python_Examples/01_Python Program to Add Two Numbers.py
@@ -0,0 +1,28 @@
+# Python Program to Add Two Numbers
+
+# Method 1. Using built-in approach
+# sum()
+# numbers = [10, 20, 30, 40]
+# result = sum(numbers)
+# print(result)
+
+# Method 2. Using two variables
+# a = 30
+# b = 40
+# sum_result = a + b
+# print("Sum is: ", sum_result)
+
+# Method 3. Using user input
+# num1 = int(input("Enter First Number: "))
+# num2 = int(input("Enter the Second Number: "))
+
+# result = num1 + num2
+# print("Sum is: ", result)
+
+# Method 4. Using a function
+def add_numbers(num1, num2):
+    return num1 + num2
+
+
+result = add_numbers(50, 60)
+print("Sum is: ", result)
